05274649282626366b5bacde274b8297c43183fd
AlexBardDev <alexandre.bardiaux@outlook.com>
2018-04-25 18:19:23 +0200

    GrandPy Bot can answer and it looks like a chat

--- static/script.js
+++ static/script.js
@@ -3,9 +3,22 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
 
+/* Some Variables */
 var height_pixels = 0;
 var myRegex = /adresse {1}[A-Za-z0-9']* ?[.?,!]{1}/;
 var incorrectQuestion = "Désolé mon enfant, mais je suis un vieux papy. Je ne comprends pas très bien ta question. Quelle adresse veux-tu ?";
+
+/* Create a new paragraph */
+function createBox(content) {
+    var pElt = document.createElement("p");
+    pElt.textContent = content;
+    pElt.style.width = "200px";
+    pElt.style.backgroundColor = "blue";
+    pElt.style.color = "white";
+    pElt.style.borderRadius = "10px";
+    pElt.style.padding = "5px";
+    document.getElementsByClassName("dialog")[0].appendChild(pElt);
+};
 
 /* Scroll automatically to the bottom of the page */
 function scroll() {
@@ -16,15 +29,14 @@ function scroll() {
 
 /* Display a new message */
 function displayMessage(content) {
-    var pElt = document.createElement("p");
-    pElt.textContent = content;
-    pElt.style.width = "200px";
-    pElt.style.backgroundColor = "blue";
-    pElt.style.color = "white";
-    pElt.style.borderRadius = "10px";
-    pElt.style.padding = "5px";
-    document.getElementsByClassName("dialog")[0].appendChild(pElt);
+    createBox(content);
     scroll();
+};
+
+/* Custom the answers of GrandPy Bot */
+function customized() {
+    var screenSize = $(".dialog").outerWidth(true) - 230;
+    $(".dialog p:last").css({"margin-left" : screenSize, "background-color" : "red"});
 };
 
 /* Search the adress and display the informations */
@@ -32,9 +44,11 @@ function searchAdress(content) {
     if (myRegex.test(content)) {
         var adress = myRegex.exec(content);
         displayMessage(adress);
+        customized();
     }
     else {
         displayMessage(incorrectQuestion);
+        customized();
     }
 };
 
